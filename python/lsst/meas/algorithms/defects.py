20d8bd9d8d36526059b68d24bbec320c6be15cc4
becker <becker@git.lsstcorp.org>
2009-03-18 01:47:44 +0000

    fixed bug with empty Defects list.

--- python/lsst/meas/algorithms/defects.py
+++ python/lsst/meas/algorithms/defects.py
@@ -9,24 +9,25 @@ def policyToBadRegionList(policyFile):
 
     badPixelsPolicy = policy.Policy.createPolicy(policyFile)
     badPixels = algorithms.DefectListT()
+
+    if badPixelsPolicy.exists("Defects"):
+        d = badPixelsPolicy.getArray("Defects")
+        for reg in d:
+            x0 = reg.get("x0")
+            width = reg.get("width")
+            if not width:
+                x1 = reg.get("x1")
+                width = x1 - x0 - 1
     
-    d = badPixelsPolicy.getArray("Defects")
-    for reg in d:
-        x0 = reg.get("x0")
-        width = reg.get("width")
-        if not width:
-            x1 = reg.get("x1")
-            width = x1 - x0 - 1
-
-        y0 = reg.get("y0")
-        height = reg.get("height")
-        if not height:
-            y1 = reg.get("y1")
-            height = y1 - y0 - 1
-
-        bbox = afwImage.BBox(afwImage.PointI(x0, y0), width, height)
-        badPixels.push_back(algorithms.Defect(bbox))
-
+            y0 = reg.get("y0")
+            height = reg.get("height")
+            if not height:
+                y1 = reg.get("y1")
+                height = y1 - y0 - 1
+    
+            bbox = afwImage.BBox(afwImage.PointI(x0, y0), width, height)
+            badPixels.push_back(algorithms.Defect(bbox))
+    
     del badPixelsPolicy
 
     return badPixels
