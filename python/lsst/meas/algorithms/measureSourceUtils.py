2c96fed8a9cf04440c1f499ce7667fa56164bcec
dubcovsky <dubcovsky@git.lsstcorp.org>
2009-08-24 22:14:30 +0000

    #743 ticket work completed, passes tests. Ready for review

--- python/lsst/meas/algorithms/measureSourceUtils.py
+++ python/lsst/meas/algorithms/measureSourceUtils.py
@@ -24,7 +24,7 @@ def showSourceSet(sSet, xy0=(0, 0), frame=0, ctype=ds9.GREEN, symb="+", size=2):
     for s in sSet:
         ds9.dot(symb, s.getXAstrom() - xy0[0], s.getYAstrom() - xy0[1], frame=frame, ctype=ctype, size=size)
 
-def makeSource(measureSources, fp, id, isNegative):
+def makeSource(measureSources, fp, sourceId, isNegative):
     source = afwDet.Source()
     source.setId(sourceId)
     detectionBits = source.getFlagForDetection() | algorithmsLib.Flags.BINNED1
@@ -48,12 +48,13 @@ def makeSourceSet(measureSources, fpPositive, fpNegative):
     """
     sourceSet = afwDet.SourceSet()
     sourceId = 0;
-    for fp in fpPositive:
-        sourceSet.append(makeSource(measureSources, fp, sourceId, False))
-        sourceId += 1
-    
-    for fp in fpNegative:
-        sourceSet.append(makeSource(measureSources, fp, sourceId, True))
-        sourceId += 1
+    if fpPositive != None:
+        for fp in fpPositive:
+            sourceSet.append(makeSource(measureSources, fp, sourceId, False))
+            sourceId += 1
+    if fpNegative != None:    
+        for fp in fpNegative:
+            sourceSet.append(makeSource(measureSources, fp, sourceId, True))
+            sourceId += 1
 
     return sourceSet
