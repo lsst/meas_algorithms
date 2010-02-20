8e96736e9e36162765dec0bd7597906db4c2e891
bick <bick@git.lsstcorp.org>
2010-02-20 22:07:54 +0000

    small bug fix for #1173, sourceMeasurement needed loop over footprintLists.

--- sourceMeasurement.py
+++ sourceMeasurement.py
@@ -8,16 +8,13 @@ import lsst.meas.algorithms   as measAlg
 import lsst.afw.detection     as afwDetection
 
 def sourceMeasurement(
-    exposure,             # exposure to analyse
-    psf,                  # psf
-    footprintLists,       # footprints of the detected objects
-    measObjPolicy,        # measureObjects policy
+    exposure,                 # exposure to analyse
+    psf,                      # psf
+    footprintSetInfoList,     # footprints of the detected objects
+    measObjPolicy,            # measureObjects policy
     ):
     """ Source Measurement """
 
-    footprintSet, isNegative = footprintLists
-    footprints = footprintSet.getFootprints()
-    
     # instantiate a measurement object for 
     # - instantiation only involves looking up the algorithms for centroid, shape, and photometry
     #   ... nothing actually gets measured yet.
@@ -26,24 +23,27 @@ def sourceMeasurement(
     # create an empty list to contain the sources we found (as Source objects)
     sourceSet = afwDetection.SourceSet()
     
-    # loop over all the objects detected
-    for i in range(len(footprints)):
+    for footprintSetInfo in footprintSetInfoList:
+        footprintSet, isNegative = footprintSetInfo
+        footprints = footprintSet.getFootprints()
 
-        # create a new source, and add it to the list, initialize ...
-        source = afwDetection.Source()
-        sourceSet.append(source)
-        source.setId(i)
+        # loop over all the objects detected
+        for i in range(len(footprints)):
 
-        # ?? not sure here
-        source.setFlagForDetection(source.getFlagForDetection() | measAlg.Flags.BINNED1);
+            # create a new source, and add it to the list, initialize ...
+            source = afwDetection.Source()
+            sourceSet.append(source)
+            source.setId(i)
 
-        # actually try to "measure" this object
-        # recall: footprints[i] is a footprint for an object, measured values will be recorded in 'source'
-        try:
-            measureSources.apply(source, footprints[i])
-        except Exception, e:
-            # logging might be nice here
-            #self.log.log(Log.WARN, str(e))
-            pass
+            source.setFlagForDetection(source.getFlagForDetection() | measAlg.Flags.BINNED1);
+
+            # actually try to "measure" this object
+            # recall: footprints[i] is a footprint for an object, measured values will be recorded in 'source'
+            try:
+                measureSources.apply(source, footprints[i])
+            except Exception, e:
+                # logging might be nice here
+                #self.log.log(Log.WARN, str(e))
+                pass
 
     return sourceSet
