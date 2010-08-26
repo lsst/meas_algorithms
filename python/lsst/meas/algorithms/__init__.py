9b3e6a71db62edc404e48348b5a88b9ec9ac0111
rhl <rhl@git.lsstcorp.org>
2010-08-26 03:06:41 +0000

    Added version() command

--- python/lsst/meas/algorithms/__init__.py
+++ python/lsst/meas/algorithms/__init__.py
@@ -21,3 +21,12 @@
 #
 
 from algorithmsLib import *
+
+import lsst.utils
+
+def version():
+    """Return current version. If a different version is setup, return that too"""
+
+    HeadURL = r"$HeadURL$"
+    return lsst.utils.version(HeadURL, "afw")
+
