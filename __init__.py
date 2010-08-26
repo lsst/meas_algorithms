55bd27e1ec2e7beb02fb586915838494816e1232
rhl <rhl@git.lsstcorp.org>
2010-08-26 07:15:55 +0000

    Added version() command

--- __init__.py
+++ __init__.py
@@ -19,7 +19,15 @@
 # the GNU General Public License along with this program.  If not, 
 # see <http://www.lsstcorp.org/LegalNotices/>.
 #
+import lsst.utils
+
+def version():
+    """Return current version. If a different version is setup, return that too"""
+
+    HeadURL = r"$HeadURL$"
+    return lsst.utils.version(HeadURL, "meas.utils")
 
 if True:
     # This "import *" is required by the way that sourceClassificationStage.py instantiates classifiers
     from sourceClassifiers import *
+
