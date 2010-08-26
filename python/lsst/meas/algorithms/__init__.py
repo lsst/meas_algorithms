4b8a28baab181ac0b46cb3d270718d6f4183748c
rhl <rhl@git.lsstcorp.org>
2010-08-26 05:32:32 +0000

    Oops.  Get the product name right

--- python/lsst/meas/algorithms/__init__.py
+++ python/lsst/meas/algorithms/__init__.py
@@ -28,5 +28,5 @@ def version():
     """Return current version. If a different version is setup, return that too"""
 
     HeadURL = r"$HeadURL$"
-    return lsst.utils.version(HeadURL, "afw")
+    return lsst.utils.version(HeadURL, "meas.algorithms")
 
