8a9797568085d29d91795cacee3bcf1781d638db
dubcovsky <dubcovsky@git.lsstcorp.org>
2010-10-28 23:35:24 +0000

    updated multifit script

--- multifit.py
+++ multifit.py
@@ -4,6 +4,7 @@ import lsst.afw.image as afwImg
 import lsst.afw.geom as afwGeom
 import lsst.pex.policy as pexPol
 import lsst.afw.geom.ellipses as geomEllipses
+import lsst.pex.exceptions
 import math
 import numpy
 import eups
@@ -13,7 +14,7 @@ def makePsModel(source):
     if(numpy.isnan(source.getApFlux()) or \
             numpy.isnan(source.getXAstrom()) or numpy.isnan(source.getYAstrom())):
         flags = source.getFlagForDetection()
-        source.setFlagForDetection(flags | measMult.Flags.FAIL_INITIALIZE_PS_NAN)
+        source.setFlagForDetection(flags | measMult.Flags.FAIL_INIT_PS_NAN)
         raise Excetion("Initial PS parameters are NaN")
 
     pixel = afwGeom.makePointD(source.getXAstrom(), source.getYAstrom())
@@ -42,9 +43,13 @@ def makeSgModel(source):
             source.getIxy())
     if quad.getDeterminant() < 0:
         #moments are bad
-        flags = source.getFlagForDetection()
+        flags= source.getFlagForDetection()
         source.setFlagForDetection(flags | measMult.Flags.FAIL_INIT_SG_MOMENTS)
         raise Exception("Initial SG moments are bad")
+    if afwGeom.ellipses.Distortion(quad).getE() > 0.95:
+        flags= source.getFlagForDetection()
+        source.setFlagForDetection(flags | measMult.Flags.FAIL_INIT_SG_DISTORTION)
+        raise Exception("high ellipticity on construction")
 
     #use fixed sersic Index
     #TODO compute this instead from source/fp instead
@@ -61,72 +66,96 @@ def makeSgModel(source):
 
     return (model, errs)
 
-def fitSourceModels(mi, psf, sources, footprints, fitterPolicy, cacheName="sersicCache.dat"):    
+def fitSourceModels(mi, psf, sources, footprints, policy, cacheName="sersicCache.boost"):    
     #some setup first. 
     #Make sure we have a cache.
     cacheDir = os.path.join(eups.productDir("meas_multifitData"), "cache")
     cachePath = os.path.join(cacheDir, cacheName)
-    cache = measMult.Cache.load(cachePath, "SersicCache", False)
+    cache = measMult.SersicCache.load(cachePath)
     measMult.SersicMorphology.setSersicCache(cache)
+   
+    fitterPol = policy.getPolicy("fitterPolicy")
+    minPixels = policy.getInt("minPixels")
+    
+    if policy.exists("fourierModelProjectionPolicy"):
+        fourierPol = policy.getPolicy("fourierModelProjectionPolicy")
+        measMult.FourierModelProjection.setPolicy(fourierPol)
 
-    fitter= measMult.MinuitNumericFitter(fitterPolicy)
+    fitter= measMult.MinuitNumericFitter(fitterPol)
     identityTransform = afwGeom.AffineTransform();
+    nSkipPs=0
+    nSkipSg=0
+    nFailPs=0
+    nFailSg=0
+    i = 0
     for s, fp in zip(sources, footprints):
+        i +=1
         flags = s.getFlagForDetection()
         try:
             psModel, psErrs = makePsModel(s)
-            psEval = measMult.ModelEvaluator(psModel)            
+            psEval = measMult.ModelEvaluator(psModel, minPixels)            
             nPix = psEval.setData(mi, psf, identityTransform, fp)
 
             if nPix > 0:
                 try:
                     psResult = fitter.apply(psEval, psErrs)
                 except:
-                    s.setFlagForDetection(flags | measMult.Flags.FAIL_FIT_PS)
-                    print "fit fail ps"
+                    flags |= measMult.Flags.FAIL_FIT_PS
+                    nFailPs += 1                    
                 else:
                     if (psResult.flags & measMult.MinuitFitterResult.MAX_ITERATION_REACHED) != 0:                    
-                        s.setFlagForDetection(flags | measMult.Flags.PS_MAX_ITERATIONS)
+                        flags |= measMult.Flags.PS_MAX_ITERATIONS
                     if (psResult.flags & measMult.MinuitFitterResult.CONVERGED) == 0:
-                        s.setFlagForDetection(flags | measMult.Flags.FAIL_FIT_PS)
+                        falgs |= measMult.Flags.FAIL_FIT_PS
+                        nFailPs += 1
                     flux = psResult.parameters[0]
                     fluxErr = math.sqrt(psResult.covariance[0,0])
                     psPhotom = measMult.PointSourceModelPhotometry(flux, fluxErr)
                     s.getPhotometry().add(psPhotom)
             else:
-                s.setFlagForDetection(flags | measMult.Flags.FAIL_INIT_PS_NO_PIX)
-                print "no pix ps"
+                flags |= measMult.Flags.FAIL_INIT_PS_NO_PIX
+                nSkipPs += 1
         except Exception, e:
-            #TODO: log this
-            print "unknown err ps", e
+            #errors in construction
+            nSkipPs += 1          
 
         try:
             sgModel, sgErrs = makeSgModel(s)
-            sgEval = measMult.ModelEvaluator(sgModel)            
+            sgEval = measMult.ModelEvaluator(sgModel, minPixels)            
             nPix= sgEval.setData(mi, psf, identityTransform, fp)
 
             if nPix > 0:
                 try:
-                    sgResult = fitter.apply(sgEval, sgErrs)                    
-                except measMult.ParameterRangeException, e:
-                    if e.notInRange(3):
-                        s.setflagsForDetection(flags | measMult.Flags.FAIL_FIT_SG_SERSIC)
-                    if e.notInRange(0) or e.notInRange(1):
-                        s.setFlagForDetection(flags | measMult.Flags.FAIL_FIT_SG_DISTORTION)
-                    print "param range sg"
+                    sgResult = fitter.apply(sgEval, sgErrs)
+                except lsst.pex.exceptions.LsstCppException, e:
+                    if e.args[0].__class__ == measMult.ParameterRangeException:
+                        if not e.args[0].isOutOfRange(0):
+                            flags |= measMult.Flags.FAIL_FIT_SG_DISTORTION
+                        if not e.args[0].isOutOfRange(3):
+                            flags |= measMult.Flags.FAIL_FIT_SG_SERSIC
+                    else:
+                        flags |= measMult.Flags.FAIL_FIT_SG_FOURIER_AREA
+                        nFailSg += 1
                 except Exception,e :
-                    s.setFlagForDetection(flags | measMult.Flags.FAIL_FIT_SG_OPTIMIZER)
-                    print "minuit sg", e
+                    falgs |= measMult.Flags.FAIL_FIT_SG_OPTIMIZER
+                    nFailSg += 1
                 else:
                     if (sgResult.flags & measMult.MinuitFitterResult.MAX_ITERATION_REACHED) != 0:                    
-                        s.setFlagForDetection(flags | measMult.Flags.SG_MAX_ITERATIONS)
+                        falgs |= measMult.Flags.SG_MAX_ITERATIONS
                     if (sgResult.flags & measMult.MinuitFitterResult.CONVERGED) == 0:
-                        s.setFlagForDetection(flags | measMult.Flags.FAIL_FIT_SG_OPTIMIZER)
-                    sgPhotom = measMult.SmallGalaxyModelPhotometry(sgResult.parameters, sgResult.covariance)
+                        nFailSg+=1
+                        flags |= measMult.Flags.FAIL_FIT_SG_OPTIMIZER
+                    sgPhotom = measMult.SmallGalaxyModelPhotometry(sgResult.parameters, sgResult.covariance, \
+                            cache.getInnerSersicRadius(), cache.getOuterSersicRadius())
                     s.getPhotometry().add(sgPhotom)
             else:           
-                s.setFlagForDetection(flags | measMult.Flags.FAIL_INIT_SG_NO_PIX)
-                print "no pix sg"
+                flags |= measMult.Flags.FAIL_INIT_SG_NO_PIX
+                nSkipSg +=1
         except Exception, e:
-            print "unknown err sg", e
-            #TODO log this
+            #errors in construction
+            nSkipSg +=1
+        
+        s.setFlagForDetection(flags)
+        
+    #return some stats about fitting
+    return (len(sources), nSkipPs, nFailPs, nSkipSg, nFailSg)
