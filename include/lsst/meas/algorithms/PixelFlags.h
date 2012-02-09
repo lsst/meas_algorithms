13fe1a19141de6dbba84cc3442359d3ffe4d3e44
Jim Bosch <jbosch@astro.princeton.edu>
2012-02-09 17:16:20 -0500

    removed old Flags that had already been #if 0'd out

--- include/lsst/meas/algorithms/PixelFlags.h
+++ include/lsst/meas/algorithms/PixelFlags.h
@@ -54,43 +54,6 @@ private:
 };
 
 
-#if 0
-/************************************************************************************************************/
-/**
- * A class to provide a set of flags describing our processing
- *
- * This would be in MeasureSources, but that's inconvenient as it's templated
- */
-struct Flags {
-    enum {
-        PEAKCENTER                      = 0x000040, ///< given centre is position of peak pixel
-        BINNED1                         = 0x000080, ///< source was found in 1x1 binned image
-        DETECT_NEGATIVE                 = 0x001000, ///< source was detected as being significantly negative
-        STAR                            = 0x002000, ///< source is thought to be point-like
-        PSFSTAR                         = 0x004000, ///< source was used in PSF determination
-
-        PHOTOM_NO_PSF                   = 0x008000, ///< NO Psf provided to photometry algorithm
-        PHOTOM_NO_PEAK                  = 0x010000, ///< NO Peak provided to photometry algorithm
-        PHOTOM_NO_SOURCE                = 0x020000, ///< NO source provided to photometry algorithm
-        PHOTOM_NO_FOOTPRINT             = 0x040000, ///< NO FOOTPRINT provided to photometry algorithm
-
-        SHAPELET_PHOTOM_NO_BASIS        = 0x080000, ///< ShapeletModelPhotometry configure without a basis
-        SHAPELET_PHOTOM_BAD_MOMENTS     = 0x100000, ///< input moments are too large or not finite
-        SHAPELET_PHOTOM_INVERSION_FAIL  = 0x200000, ///< ShapeletModelPhotometry failed
-        SHAPELET_PHOTOM_INVERSION_UNSAFE= 0x400000, ///< ShapeletModelPhotometry should not be trusted
-        SHAPELET_PHOTOM_GALAXY_FAIL     = 0x800000, ///< ShapeletModelPhotometry only fit a point source model
-        ///ShapeletModelPhtoometry should be ignored in essentially all analyses
-        SHAPELET_PHOTOM_BAD = PHOTOM_NO_PSF | PHOTOM_NO_SOURCE | PHOTOM_NO_FOOTPRINT | 
-                SHAPELET_PHOTOM_NO_BASIS | SHAPELET_PHOTOM_BAD_MOMENTS | 
-                SHAPELET_PHOTOM_INVERSION_FAIL | SHAPELET_PHOTOM_INVERSION_UNSAFE,
-        /// Should this this object be ignored in essentially all analyses?
-        BAD                       = EDGE|INTERP_CENTER|SATUR_CENTER
-
-    };
-};
-
-#endif
-
 }}} // namespace
 
 #endif
