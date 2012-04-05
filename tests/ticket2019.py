8765342b547bb12d44cc61a0cd17584587e6a842
Dustin Lang <dstn@astro.princeton.edu>
2012-04-05 12:05:30 -0500

    Find the sources and add assertions

--- tests/ticket2019.py
+++ tests/ticket2019.py
@@ -11,6 +11,8 @@ import lsst.meas.algorithms as measAlg
 
 import lsst.meas.extensions.shapeHSM
 
+import numpy as np
+
 plots = True
 if plots:
     try:
@@ -48,6 +50,12 @@ def plotSources(im, sources, schema):
     plt.gray()
     shapekey = schema.find('shape.sdss').key
     xykey = schema.find('centroid.sdss').key
+
+    flagkeys = [schema.find(x).key for x in [
+        'shape.sdss.flags.maxiter', 'shape.sdss.flags.shift',
+        'shape.sdss.flags.unweighted', 'shape.sdss.flags.unweightedbad']]
+    flagabbr = ['M','S','U','B']
+
     for source in sources:
         quad = source.get(shapekey)
         ixx,iyy = quad.getIxx(), quad.getIyy()
@@ -55,41 +63,55 @@ def plotSources(im, sources, schema):
         sx,sy = sqrt(ixx),sqrt(iyy)
         plt.gca().add_artist(Ellipse([x,y], 2.*sx, 2.*sy, angle=0.,
                                      ec='r', fc='none', lw=2, alpha=0.5))
+        fs = ''
+        for j,key in enumerate(flagkeys):
+            val = source.get(key)
+            if val:
+                fs += flagbbr[j]
+        if len(fs):
+            plt.text(x+5, y, fs, ha='left', va='center')
+        
     
 
 class RemoveOtherSourcesTestCase(unittest.TestCase):
 
     def test1(self):
+        seed = 42
+        rand = afwMath.Random(afwMath.Random.MT19937, seed)
+        
+        #for k in range(5):
+        self.runone(1, rand)
+
+    def runone(self, kk, rand):
         FWHM = 5
         ksize = 25
         psf = afwDet.createPsf("DoubleGaussian", ksize, ksize,
                                FWHM/(2*sqrt(2*log(2))), 1, 0.1)
 
         im = afwImage.ImageF(120, 200)
-
-        sky = 100
-        rand = afwMath.Random()
+        skystd = 100
         afwMath.randomGaussianImage(im, rand)
-        im *= sky
+        im *= skystd
 
         x = 60
-        #for dx,y in [(30, 20), (35, 40), (40, 60), (45, 80)]:
-        for i in range(5):
-            dx = 28 + i*4
-            #dx = 24 + i*4
-            y = 20 + i*40
+        y0 = 16
+        ystep = 33
+        for i in range(6):
+            dx = [28,29,30, 35,36,37][i]
+            y = y0 + i*ystep
             #                x   y  sx sy flux
-            #addGaussian(im, x, y, 10, 3, 1.5e5)
-            addGaussian(im, x, y, 10, 3, 1e5)
+            addGaussian(im, x, y, 10, 3, 2e5)
+            #addGaussian(im, x, y, 10, 3, 1e5)
             addPsf(im, psf, x+dx, y, 1000)
             addPsf(im, psf, x-dx, y, 1000)
 
-        im.writeFits('im.fits')
+        #im.writeFits('im.fits')
 
+        # This doesn't seem to be used...
         mi = afwImage.MaskedImageF(im)
         var = mi.getVariance()
-        print 'Variance:', var
-        var = sky
+        #print 'Variance:', var
+        var = skystd**2
         exposure = afwImage.makeExposure(mi)
         exposure.setPsf(psf)
 
@@ -99,8 +121,8 @@ class RemoveOtherSourcesTestCase(unittest.TestCase):
         measconf = measAlg.SourceMeasurementConfig()
         measconf.doApplyApCorr = False
 
-        newalgs = [ 'shape.hsm.ksb', 'shape.hsm.bj', 'shape.hsm.linear' ]
-        measconf.algorithms = list(measconf.algorithms.names) + newalgs
+        #newalgs = [ 'shape.hsm.ksb', 'shape.hsm.bj', 'shape.hsm.linear' ]
+        #measconf.algorithms = list(measconf.algorithms.names) + newalgs
         #measconf.algorithms += newalgs
 
         schema = afwTable.SourceTable.makeMinimalSchema()
@@ -112,54 +134,82 @@ class RemoveOtherSourcesTestCase(unittest.TestCase):
         detected = detect.makeSourceCatalog(table, exposure)
         sources = detected.sources
 
-        print 'Running measurement...'
-        measure.run(exposure, sources)
+        self.assertEqual(len(sources), 18)
 
-        #fields = schema.getNames()
-        #print 'Fields:', fields
-        fields = ['centroid.sdss', 'shape.sdss',
-                  'shape.hsm.bj.moments',
-                  'shape.hsm.ksb.moments',
-                  'shape.hsm.linear.moments',
-                  ]
-        keys = [schema.find(f).key for f in fields]
-        #print 'Fields:', fields
-        #print 'Keys:', keys
-        for source in sources:
-            print '  ', source
-            for f,k in zip(fields, keys):
-                val = source.get(k)
-                print '    ', f, val
+        for jj in range(2):
+
+            print 'Running measurement...'
+            measure.run(exposure, sources)
+
+            # fields = schema.getNames()
+            # print 'Fields:', fields
+            fields = ['centroid.sdss', 'shape.sdss',
+                      #'shape.hsm.bj.moments',
+                      #'shape.hsm.ksb.moments',
+                      #'shape.hsm.linear.moments',
+                      #'shape.sdss.flags.maxiter', 'shape.sdss.flags.shift',
+                      #'shape.sdss.flags.unweighted', 'shape.sdss.flags.unweightedbad'
+                      ]
+            keys = [schema.find(f).key for f in fields]
+            xx,yy,vx,vy = [],[],[],[]
+            for source in sources:
+                #print '  ', source
+                #for f,k in zip(fields, keys):
+                #    val = source.get(k)
+                #    print '    ', f, val
+                xx.append(source.getX())
+                yy.append(source.getY())
+                vx.append(source.getIxx())
+                vy.append(source.getIyy())
         
+            if plots:
+                plotSources(im, sources, schema)
+                plt.savefig('%i%s.png' % (kk, chr(ord('a')+jj)))
 
-        if plots:
-            plotSources(im, sources, schema)
-            plt.savefig('1a.png')
+            #print 'vx:', vx
+            # Sort, first vertically then horizontally
+            iy = [int(round((y - y0) / float(ystep))) for y in yy]
+            iy = np.array(iy)
+            xx = np.array(xx)
+            vx = np.array(vx)
+            vy = np.array(vy)
+            I = np.argsort(iy * 1000 + xx)
+            vx = vx[I]
+            vy = vy[I]
+            #print 'vx', vx
+            vx = vx[slice(1, 18, 3)]
+            #print 'vx', vx
 
-        measconf.doRemoveOtherSources = True
-        print 'Running measurement...'
-        measure.run(exposure, sources)
+            bad = vx[:3]
+            good = vx[3:]
 
-        fields = schema.getNames()
-        print 'Fields:', fields
-        fields = ['centroid.sdss', 'shape.sdss',
-                  'shape.hsm.bj.moments',
-                  'shape.hsm.ksb.moments',
-                  'shape.hsm.linear.moments',
-                  #'flags.pixel',
-                  ]
-        keys = [schema.find(f).key for f in fields]
-        #print 'Fields:', fields
-        #print 'Keys:', keys
-        for source in sources:
-            print '  ', source
-            for f,k in zip(fields, keys):
-                val = source.get(k)
-                print '    ', f, val
+            # When SdssShape fails, we get variance ~ 11
 
-        if plots:
-            plotSources(im, sources, schema)
-            plt.savefig('1b.png')
+            I = np.flatnonzero(bad > 50.)
+            self.assertTrue(len(I) > 0)
+            bad = bad[I]
+            I = np.flatnonzero(good > 50.)
+            self.assertTrue(len(I) > 0)
+            good = good[I]
+
+            print 'bad:', bad
+            print 'good:', good
+
+            # bad: [ 209.78476672  192.35271583  176.76274525]
+            # good: [  99.40557099  110.5701382 ]
+
+            oklo,okhi = 80,120
+            self.assertTrue(all((good > oklo) * (good < okhi)))
+            if jj == 0:
+                # Without "doRemoveOtherSources", we expect to find the variances
+                # overestimated.
+                self.assertTrue(all(bad > okhi))
+            else:
+                # With "doRemoveOtherSources", no problem!
+                self.assertTrue(all((bad > oklo) * (bad < okhi)))
+
+            measconf.doRemoveOtherSources = True
+
 
 
 
