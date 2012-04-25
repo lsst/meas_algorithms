8273b8ea7ce444445930640d1e00c9af03b2817b
Dustin Lang <dstn@astro.princeton.edu>
2012-04-25 14:29:12 -0500

    Make debugging plots for removing other sources before measuring

--- examples/drivers/ticket2019-subaru.py
+++ examples/drivers/ticket2019-subaru.py
@@ -1,3 +1,9 @@
+debugPlots = True
+if debugPlots:
+    import matplotlib
+    matplotlib.use('Agg')
+
+
 import lsst.pipe.base as pipeBase
 import lsst.pipe.tasks.processCcd as procCcd
 import lsst.daf.persistence as dafPersist
@@ -8,9 +14,69 @@ import lsst.afw.table as afwTable
 from lsst.ip.isr import IsrTask
 from lsst.pipe.tasks.calibrate import CalibrateTask
 
-def printConfig(conf):
-    for k,v in conf.items():
-        print '  ', k, v
+if debugPlots:
+    #
+    # Plot the image that will be passed to the measurement algorithms,
+    # for a few bright sources in a small region.
+    #
+    import pylab as plt
+    import numpy as np
+    import lsst.meas.algorithms as measAlg
+    import lsst.afw.math as afwMath
+    class DebugSourceMeasTask(measAlg.SourceMeasurementTask):
+        def __str__(self):
+            return 'DebugSourceMeasTask'
+        def run(self, *args, **kwargs):
+            print 'DebugSourceMeasTask running.'
+            super(DebugSourceMeasTask,self).run(*args, **kwargs)
+        def _plotimage(self, im):
+            xlo,xhi,ylo,yhi = self.plotregion
+            plt.clf()
+            plt.imshow(im.getArray()[ylo:yhi, xlo:xhi],
+                       extent=[xlo,xhi,ylo,yhi], **self.plotargs)
+            plt.gray()
+            
+        def preMeasureHook(self, exposure, sources):
+            mi = exposure.getMaskedImage()
+            im = mi.getImage()
+            s = afwMath.makeStatistics(im, afwMath.STDEVCLIP + afwMath.MEANCLIP)
+            mn = s.getValue(afwMath.MEANCLIP)
+            std = s.getValue(afwMath.STDEVCLIP)
+            lo = mn -  3*std
+            hi = mn + 20*std
+            self.plotargs = dict(interpolation='nearest', origin='lower',
+                                 vmin=lo, vmax=hi)
+            self.plotregion = (100, 500, 100, 500)
+            self.nplots = 0
+            self._plotimage(im)
+            plt.savefig('pre.png')
+        def postMeasureHook(self, exposure, sources):
+            mi = exposure.getMaskedImage()
+            im = mi.getImage()
+            self._plotimage(im)
+            plt.savefig('post.png')
+        def postSingleMeasureHook(self, exposure, sources, i):
+            xlo,xhi,ylo,yhi = self.plotregion
+            x,y = sources[i].getX(), sources[i].getY()
+            if x < xlo or x > xhi or y < ylo or y > yhi:
+                return
+            if (not np.isfinite(x)) or (not np.isfinite(y)):
+                return
+            if sources[i].getPsfFlux() < 20000.:
+                return
+            print 'Source at', x,y
+            print 'PSF flux', sources[i].getPsfFlux()
+            #print 'model flux', sources[i].getModelFlux()
+            #print 'ap flux', sources[i].getApFlux()
+            #print 'inst flux', sources[i].getInstFlux()
+            if self.nplots >= 5:
+                return
+            mi = exposure.getMaskedImage()
+            im = mi.getImage()
+            self._plotimage(im)
+            plt.savefig('meas%02i.png' % self.nplots)
+            self.nplots += 1
+
 
 if __name__ == '__main__':
     mapperArgs = dict(root='/lsst/home/dstn/lsst/ACT-data/rerun/dstn',
@@ -46,7 +112,15 @@ if __name__ == '__main__':
     cr.niteration = 3
     cr.nCrPixelMax = 1000000
 
+    if debugPlots:
+        conf.doMeasurement = False
+
     proc = procCcd.ProcessCcdTask(config=conf, name='ProcessCcd')
+    if debugPlots:
+        proc.measurement = DebugSourceMeasTask(proc.schema,
+                                               algMetadata=proc.algMetadata,
+                                               config=conf.measurement)
+        conf.doMeasurement = True
 
     conf.calibrate.measurement.doApplyApCorr = False
     conf.measurement.doApplyApCorr = False
@@ -74,5 +148,6 @@ if __name__ == '__main__':
         conf.doIsr = doIsr
         conf.doCalibrate = doCalib
 
+        print 'proc.run'
         proc.run(dr)
     
