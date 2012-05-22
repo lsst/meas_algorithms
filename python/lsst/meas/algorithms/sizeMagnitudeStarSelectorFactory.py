4e881877f8df09f026dee7798e95eb85d08ae813
Robert Lupton the Good <rhl@astro.princeton.edu>
2012-05-22 12:03:27 -0400

    Changes to make SizeMagnitudeStarSelector more usable

--- python/lsst/meas/algorithms/sizeMagnitudeStarSelectorFactory.py
+++ python/lsst/meas/algorithms/sizeMagnitudeStarSelectorFactory.py
@@ -25,57 +25,57 @@ from .algorithmsLib import SizeMagnitudeStarSelector
 __all__ = ("sizeMagnitudeStarSelectorFactory",)
 
 class SizeMagnitudeStarSelectorConfig(pexConfig.Config):
-    minSize = pexConfig.Field(
+    minsize = pexConfig.Field(
         doc = "Minimum size to use",
         dtype = float,
         default = 0.0,
     )
-    maxSize = pexConfig.Field(
+    maxsize = pexConfig.Field(
         doc = "Maximum size to use",
         dtype = float,
         default = 1.0e100,
     )
-    isSizeLog = pexConfig.Field(
+    logsize = pexConfig.Field(
         doc = "Are sizes already log(size)?",
         dtype = bool,
         default = False,
     )
-    minMag = pexConfig.Field(
+    minmag = pexConfig.Field(
         doc = "Minimum magnitude to use",
         dtype = float,
         default = 0.0,
     )
-    maxMag = pexConfig.Field(
+    maxmag = pexConfig.Field(
         doc = "Maximum magnitude to use",
         dtype = float,
         default = 1.0e100,
     )
-    starFrac = pexConfig.Field(
+    starfrac = pexConfig.Field(
         doc = "What fraction of objects are likely stars?",
         dtype = float,
         default = 0.5,
     )
-    startN = pexConfig.Field(
+    startn1 = pexConfig.Field(
         doc = "Fraction of objects to use in first pass",
         dtype = float,
         default = 0.1,
     )
-    fitOrder = pexConfig.Field(
+    fitorder = pexConfig.Field(
         doc = "Order of polynomial of fit of size(x,y)",
         dtype = int,
         default = 1,
     )
-    fitSigClip = pexConfig.Field(
+    fitsigclip = pexConfig.Field(
         doc = "nSigma to reject a star as an outlier",
         dtype = float,
         default = 4.0,
     )
-    fitStars = pexConfig.Field(
+    starsperbin = pexConfig.Field(
         doc = "Perform size(x,y) fit with fitStars brightest stars",
         dtype = int,
         default = 30,
     )
-    purity = pexConfig.Field(
+    purityratio = pexConfig.Field(
         doc = "Smaller = purer smaple of stars, larger = more stars",
         dtype = float,
         default = 0.05,
