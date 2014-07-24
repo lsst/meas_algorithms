65f617089038fe19179fca4f959bf23ea061a6b8
Naoki Yasuda <yasudank@gmail.com>
2014-07-24 16:09:49 +0900

    Test case for measurement of negative objects.

---
+++ tests/negative.py
@@ -0,0 +1,109 @@
+import unittest
+
+import lsst.afw.image as afwImage
+import lsst.afw.geom as afwGeom
+import lsst.afw.table as afwTable
+import lsst.meas.algorithms as algorithms
+import lsst.utils.tests as utilsTests
+import lsst.afw.detection.detectionLib as afwDetection
+from lsst.meas.algorithms import SourceDetectionTask, SourceMeasurementTask
+from lsst.meas.algorithms.testUtils import plantSources
+import lsst.daf.base                    as dafBase
+
+import lsst.afw.display.ds9 as ds9
+
+class NegativeMeasurementTestCase(unittest.TestCase):
+    """A test case for negative objects"""
+    def testBasics(self):
+        bbox = afwGeom.Box2I(afwGeom.Point2I(256, 100), afwGeom.Extent2I(128, 127))
+        minCounts = 2000
+        maxCounts = 20000
+        starSigma = 1.5
+        numX = 4
+        numY = 4
+        coordList = self.makeCoordList(
+            bbox = bbox,
+            numX = numX,
+            numY = numY,
+            minCounts = minCounts,
+            maxCounts = maxCounts,
+            sigma = starSigma,
+        )
+        kwid = 11
+        sky = 2000
+        addPoissonNoise=True
+        exposure = plantSources(bbox=bbox, kwid=kwid, sky=sky, coordList=coordList,
+            addPoissonNoise=addPoissonNoise)
+
+        schema = afwTable.SourceTable.makeMinimalSchema()        
+        config = SourceDetectionTask.ConfigClass()
+        config.reEstimateBackground = False
+        config.thresholdPolarity = 'both'
+        detection = SourceDetectionTask(config=config, schema=schema)
+        algMetadata = dafBase.PropertyList()
+        measurement = SourceMeasurementTask(schema=schema, algMetadata=algMetadata)
+
+        table = afwTable.SourceTable.make(schema)
+        detections = detection.makeSourceCatalog(table, exposure)
+        sources = detections.sources
+        fpSets = detections.fpSets
+
+        self.assertEqual(len(sources), numX * numY)
+        self.assertEqual(fpSets.numPos, numX * numY / 2)
+        self.assertEqual(fpSets.numNeg, numX * numY / 2)
+
+        measurement.run(exposure, sources)
+
+        nGoodCent = 0
+        nGoodShape = 0
+        for s in sources:
+            cent = s.get('centroid.sdss')
+            shape = s.get('shape.sdss')
+
+            if cent[0] == cent[0] and cent[1] == cent[1]:
+                nGoodCent += 1
+
+            if (shape.getIxx() == shape.getIxx() and
+                shape.getIyy() == shape.getIyy() and
+                shape.getIxy() == shape.getIxy()):
+                nGoodShape += 1
+
+        self.assertEqual(nGoodCent, numX * numY)
+        self.assertEqual(nGoodShape, numX * numY)
+
+    def makeCoordList(self, bbox, numX, numY, minCounts, maxCounts, sigma):
+        """Make a coordList for makeExposure"""
+        dX = bbox.getWidth() / float(numX)
+        dY = bbox.getHeight() / float(numY)
+        minX = bbox.getMinX() + (dX / 2.0)
+        minY = bbox.getMinY() + (dY / 2.0)
+        dCounts = (maxCounts - minCounts) / (numX * numY / 2 - 1)
+    
+        coordList = []
+        counts = minCounts
+        for i in range(numX):
+            x = minX + (dX * i)
+            for j in range(numY):
+                y = minY + (dY * j)
+                if j % 2 == 0:
+                    coordList.append([x, y, counts, sigma])
+                else:
+                    coordList.append([x, y, -counts, sigma])
+                    counts += dCounts
+        return coordList
+
+def suite():
+    """Returns a suite containing all the test cases in this module."""
+    utilsTests.init()
+
+    suites = []
+    suites += unittest.makeSuite(NegativeMeasurementTestCase)
+
+    return unittest.TestSuite(suites)
+
+def run(exit = False):
+    """Run the tests"""
+    utilsTests.run(suite(), exit)
+
+if __name__ == "__main__":
+    run(True)
