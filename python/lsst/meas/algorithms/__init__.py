ce18b35dd82aa74e8c92c90c5fc548bcf8d509d9
Jim Bosch <jbosch@astro.princeton.edu>
2015-03-18 17:20:58 -0400

    Remove ReplaceWithNoiseTask and its test code (DM-420 leftover)
    This functionality has now been moved entirely to meas_base, where we already
    have stronger and simpler tests for its behavior.

--- python/lsst/meas/algorithms/__init__.py
+++ python/lsst/meas/algorithms/__init__.py
@@ -27,7 +27,6 @@ from .starSelectorRegistry import *
 from .findCosmicRaysConfig import *
 from .detection import *
 from .gaussianPsfFactory import *
-from .replaceWithNoise import *
 from .loadReferenceObjects import *
 from . import objectSizeStarSelector  # don't need names, just registration
 
