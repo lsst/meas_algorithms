c36baa5cf5ab2448af9aa52b7194b0261d2dcb7e
Russell Owen <rowen@uw.edu>
2016-03-18 16:54:04 -0700

    Add StarSelectorTask
    Add StarSelectorTask as a base class for star selectors;
    this introduces a new API as per RFC-154:
    - selectStars returns a struct containing a catalog of stars
    instead of a list of PSF candidates
    - makePsfCandidates makes candidates from a catalog of stars
    - run calls selectStars and optionally sets a flag field

--- python/lsst/meas/algorithms/starSelector.py
+++ python/lsst/meas/algorithms/starSelector.py
@@ -19,11 +19,129 @@
 # the GNU General Public License along with this program.  If not, 
 # see <http://www.lsstcorp.org/LegalNotices/>.
 #
-from lsst.pex.config import makeRegistry
+from __future__ import absolute_import, division, print_function
 
-__all__ = ["starSelectorRegistry"]
+import abc
 
-starSelectorRegistry = makeRegistry(
+import numpy as np
+
+import lsst.afw.math as afwMath
+import lsst.pex.config as pexConfig
+import lsst.pipe.base as pipeBase
+from . import algorithmsLib
+
+__all__ = ["StarSelectorConfig", "StarSelectorTask", "starSelectorRegistry"]
+
+class StarSelectorConfig(pexConfig.Config):
+    kernelSize = pexConfig.Field(
+        doc = "size of the kernel to create",
+        dtype = int,
+        default = 21,
+    )
+    borderWidth = pexConfig.Field(
+        doc = "number of pixels to ignore around the edge of PSF candidate postage stamps",
+        dtype = int,
+        default = 0,
+    )
+    badFlags = pexConfig.ListField(
+        doc = "List of flags which cause a source to be rejected as bad",
+        dtype = str,
+        default = [
+            "base_PixelFlags_flag_edge",
+            "base_PixelFlags_flag_interpolatedCenter",
+            "base_PixelFlags_flag_saturatedCenter",
+            "base_PixelFlags_flag_crCenter",
+            "base_PixelFlags_flag_bad",
+            "base_PixelFlags_flag_interpolated",
+        ],
+    )
+
+
+class StarSelectorTask(pipeBase.Task):
+    """!Base class for star selectors
+
+    Register all star selectors with the starSelectorRegistry using:
+        starSelectorRegistry.register(name, class)
+    """
+    __metaclass__ = abc.ABCMeta
+
+    usesMatches = False # Does the star selector use the "matches" argument in the "run method? Few do.
+    ConfigClass = StarSelectorConfig
+    _DefaultName = "starSelector"
+
+    def run(self, exposure, sourceCat, matches=None, isStarField=None):
+        """!Select stars and optionally set a flag field True in the input catalog for stars
+
+        @param[in] exposure  the exposure containing the sources
+        @param[in] sourceCat  catalog of sources that may be stars (an lsst.afw.table.SourceCatalog)
+        @param[in] matches  astrometric matches; ignored by this star selector
+                (an lsst.afw.table.ReferenceMatchVector), or None. Some star selectors
+                will ignore this argument, others may require it. See the usesMatches class variable.
+        @param[in] isStarField  name of flag field to set True for stars, or None to not set a field;
+            the field is left unchanged for non-stars
+
+        @return an lsst.pipe.base.Struct containing:
+        - starCat  catalog of selected stars (a subset of sourceCat)
+        """
+        retVal = self.selectStars(exposure=exposure, sourceCat=sourceCat, matches=matches)
+
+        if isStarField is not None:
+            isStarKey = sourceCat.schema[isStarField].asKey()
+            for star in retVal.starCat:
+                star.set(isStarKey, True)
+
+        return retVal
+
+    @abc.abstractmethod
+    def selectStars(self, exposure, sourceCat, matches=None):
+        """!Return a catalog of stars: a subset of sourceCat
+        
+        A list of PSF candidates may be used by a PSF fitter to construct a PSF.
+        
+        @param[in] exposure  the exposure containing the sources
+        @param[in] sourceCat  catalog of sources that may be stars (an lsst.afw.table.SourceCatalog)
+        @param[in] matches  astrometric matches; ignored by this star selector
+                (an lsst.afw.table.ReferenceMatchVector), or None. Some star selectors
+                will ignore this argument, others may require it. See the usesMatches class variable.
+        
+        @return a pipeBase.Struct containing:
+        - starCat  a catalog of stars
+        """
+        raise NotImplementedError("StarSelectorTask is abstract, subclasses must override this method")
+
+    def makePsfCandidates(self, exposure, starCat):
+        """!Make a list of PSF candidates from a star catalog
+
+        @param[in] exposure  the exposure containing the sources
+        @param[in] starCat  catalog of stars (an lsst.afw.table.SourceCatalog),
+                            e.g. as returned by the run or selectStars method
+        """
+        psfCandidateList = []
+        for star in starCat:
+            try:
+                psfCandidate = algorithmsLib.makePsfCandidate(star, exposure)
+            except Exception as err:
+                self.log.warn("Failed to make a psfCandidate from star %d: %s" % (star.getId(), err))
+                continue
+                
+            # The setXXX methods are class static, but it's convenient to call them on
+            # an instance as we don't know Exposure's pixel type
+            # (and hence psfCandidate's exact type)
+            if psfCandidate.getWidth() == 0:
+                psfCandidate.setBorderWidth(self.config.borderWidth)
+                psfCandidate.setWidth(self.config.kernelSize + 2*self.config.borderWidth)
+                psfCandidate.setHeight(self.config.kernelSize + 2*self.config.borderWidth)
+
+            im = psfCandidate.getMaskedImage().getImage()
+            vmax = afwMath.makeStatistics(im, afwMath.MAX).getValue()
+            if not np.isfinite(vmax):
+                continue
+            psfCandidateList.append(psfCandidate)
+
+        return psfCandidateList
+
+
+starSelectorRegistry = pexConfig.makeRegistry(
     '''A registry of star selectors
     
         A star selector is a class with the following class variables and methods:
