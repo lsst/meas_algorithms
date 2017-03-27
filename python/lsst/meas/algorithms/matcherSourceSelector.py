94baf06f16ec38657b8ed9d8f929bcf3de00c419
morriscb@uw.edu <morriscb@uw.edu>
2017-03-27 15:49:15 -0700

    Fixed linting on matcherSourceSelector.py

--- python/lsst/meas/algorithms/matcherSourceSelector.py
+++ python/lsst/meas/algorithms/matcherSourceSelector.py
@@ -1,8 +1,8 @@
-# 
+#
 # LSST Data Management System
 #
 # Copyright 2008-2017  AURA/LSST.
-# 
+#
 # This product includes software developed by the
 # LSST Project (http://www.lsst.org/).
 #
@@ -10,14 +10,14 @@
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
-# You should have received a copy of the LSST License Statement and 
-# the GNU General Public License along with this program.  If not, 
+#
+# You should have received a copy of the LSST License Statement and
+# the GNU General Public License along with this program.  If not,
 # see <https://www.lsstcorp.org/LegalNotices/>.
 #
 from __future__ import absolute_import, division, print_function
@@ -26,7 +26,9 @@ import numpy as np
 
 from lsst.afw import table
 import lsst.pex.config as pexConfig
-from .sourceSelector import BaseSourceSelectorConfig, BaseSourceSelectorTask, sourceSelectorRegistry
+from .sourceSelector import BaseSourceSelectorConfig
+from .sourceSelector import BaseSourceSelectorTask
+from .sourceSelector import sourceSelectorRegistry
 from lsst.pipe.base import Struct
 from functools import reduce
 
@@ -39,8 +41,9 @@ class MatcherSourceSelectorConfig(BaseSourceSelectorConfig):
     )
     minSnr = pexConfig.Field(
         dtype=float,
-        doc="Minimum allowed signal-to-noise ratio for sources used for matching "
-        "(in the flux specified by sourceFluxType); <= 0 for no limit",
+        doc="Minimum allowed signal-to-noise ratio for sources used for "
+            "matching (in the flux specified by sourceFluxType); <= 0 for no "
+            "limit.",
         default=40,
     )
 
@@ -49,8 +52,8 @@ class MatcherSourceSelectorTask(BaseSourceSelectorTask):
     """
     !Select sources that are useful for matching.
 
-    Good matching sources have high signal/noise, are non-blended. They need not
-    be PSF sources, just have reliable centroids.
+    Good matching sources have high signal/noise, are non-blended. They need
+    not be PSF sources, just have reliable centroids.
     """
     ConfigClass = MatcherSourceSelectorConfig
 
@@ -61,9 +64,8 @@ class MatcherSourceSelectorTask(BaseSourceSelectorTask):
         """
         !Return a catalog of sources: a subset of sourceCat.
 
-        If sourceCat is cotiguous in memory, will use vectorized tests for ~100x
-        execution speed advantage over non-contiguous catalogs. This would be
-        even faster if we didn't have to check footprints for multiple peaks.
+        If sourceCat is cotiguous in memory, will use vectorized tests for
+        ~100x execution speed advantage over non-contiguous catalogs.
 
         @param[in] sourceCat  catalog of sources that may be sources
                                 (an lsst.afw.table.SourceCatalog)
@@ -97,14 +99,14 @@ class MatcherSourceSelectorTask(BaseSourceSelectorTask):
         self.fluxSigmaKey = schema[fluxPrefix + "fluxSigma"].asKey()
 
         self.edgeKey = schema["base_PixelFlags_flag_edge"].asKey()
-        self.interpolatedCenterKey = schema["base_PixelFlags_flag_interpolatedCenter"].asKey()
+        self.interpolatedCenterKey = schema[
+            "base_PixelFlags_flag_interpolatedCenter"].asKey()
         self.saturatedKey = schema["base_PixelFlags_flag_saturated"].asKey()
 
     def _isGood_vector(self, sourceCat):
-        test = ~sourceCat.get(self.saturatedKey) & \
+        return ~sourceCat.get(self.saturatedKey) & \
                ~sourceCat.get(self.interpolatedCenterKey) & \
                ~sourceCat.get(self.edgeKey)
-        return test
 
     def _isGood(self, source):
         return not source.get(self.saturatedKey) and \
@@ -113,8 +115,7 @@ class MatcherSourceSelectorTask(BaseSourceSelectorTask):
 
     def _isParent_vector(self, sourceCat):
         """Return True for each source that is the parent source."""
-        test = (sourceCat.get(self.parentKey) == 0)
-        return test
+        return (sourceCat.get(self.parentKey) == 0)
 
     def _isParent(self, source):
         """Return True if source is the parent source."""
@@ -134,16 +135,20 @@ class MatcherSourceSelectorTask(BaseSourceSelectorTask):
         return np.all(np.isfinite(centroid)) and not source.getCentroidFlag()
 
     def _goodSN_vector(self, sourceCat):
-        """Return True for each source that has Signal/Noise > config.minSnr."""
+        """Return True for each source that has Signal/Noise > config.minSnr.
+        """
         if self.config.minSnr <= 0:
             return True
         else:
-            return sourceCat.get(self.fluxKey)/sourceCat.get(self.fluxSigmaKey) > self.config.minSnr
+            return (sourceCat.get(self.fluxKey) /
+                    sourceCat.get(self.fluxSigmaKey) >
+                    self.config.minSnr)
 
     def _goodSN(self, source):
         """Return True if source has Signal/Noise > config.minSnr."""
         return (self.config.minSnr <= 0 or
-                (source.get(self.fluxKey)/source.get(self.fluxSigmaKey) > self.config.minSnr))
+                (source.get(self.fluxKey) /
+                 source.get(self.fluxSigmaKey) > self.config.minSnr))
 
     def _isUsable_vector(self, sourceCat):
         """
@@ -153,39 +158,8 @@ class MatcherSourceSelectorTask(BaseSourceSelectorTask):
         For a source to be usable it must:
         - have a valid centroid
         - not be deblended
-        - have a valid flux (of the type specified in this object's constructor)
-        - have adequate signal-to-noise
-        """
-        return self._hasCentroid_vector(sourceCat) \
-            & self._isParent_vector(sourceCat) \
-            & self._goodSN_vector(sourceCat) \
-            & ~sourceCat.get(self.fluxFlagKey)
-
-    def _isUsable(self, source):
-        """
-        Return True if the source is usable for matching, even if it may have a
-        poor centroid.
-
-        For a source to be usable it must:
-        - have a valid centroid
-        - not be deblended
-        - have a valid flux (of the type specified in this object's constructor)
-        - have adequate signal-to-noise
-        """
-        return self._hasCentroid(source) \
-            and self._isParent(source) \
-            and not source.get(self.fluxFlagKey) \
-            and self._goodSN(source)
-
-    def _isUsable_vector(self, sourceCat):
-        """
-        Return True for each source that is usable for matching, even if it may
-        have a poor centroid.
-
-        For a source to be usable it must:
-        - have a valid centroid
-        - not be deblended
-        - have a valid flux (of the type specified in this object's constructor)
+        - have a valid flux (of the type specified in this object's
+            constructor)
         - have adequate signal-to-noise
         """
         return self._hasCentroid_vector(sourceCat) \
@@ -202,7 +176,8 @@ class MatcherSourceSelectorTask(BaseSourceSelectorTask):
         For a source to be usable it must:
         - have a valid centroid
         - not be deblended
-        - have a valid flux (of the type specified in this object's constructor)
+        - have a valid flux (of the type specified in this object's
+            constructor)
         - have adequate signal-to-noise
         """
         return self._hasCentroid(source) \
