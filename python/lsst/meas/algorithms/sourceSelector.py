4c082160df4ba0b1ea3e50bfb10ddd07f4c17b42
Christopher Morrison <morriscb@uw.edu>
2017-05-30 14:37:48 -0700

    Removed non-contigous case.

--- python/lsst/meas/algorithms/sourceSelector.py
+++ python/lsst/meas/algorithms/sourceSelector.py
@@ -40,7 +40,7 @@ from future.utils import with_metaclass
 
 
 class BaseSourceSelectorConfig(pexConfig.Config):
-    badFlags = pexConfig.ListField(
+    bad_flags = pexConfig.ListField(
         doc="List of flags which cause a source to be rejected as bad",
         dtype=str,
         default=[
@@ -53,51 +53,122 @@ class BaseSourceSelectorConfig(pexConfig.Config):
     )
 
 
-class BaseSourceSelectorTask(with_metaclass(abc.ABCMeta, pipeBase.Task)):
-    """!Base class for source selectors
+@with_metaclass(abc.ABCMeta)
+class BaseSourceSelectorTask(pipeBase.Task):
+    """Base class for source selectors
+
+    Write paragraph about what a source selector is/does.
 
     Register all source selectors with the sourceSelectorRegistry using:
         sourceSelectorRegistry.register(name, class)
+
+    Attributes
+    ----------
+    usesMatches : bool
+        A boolean variable specifiy if the inherited source selector uses
+        matches.
     """
 
     ConfigClass = BaseSourceSelectorConfig
-    _DefaultName = "sourceSelector"
+    _DefaultName = "source_selector"
+    uses_matches = False
 
     def __init__(self, **kwargs):
-        """!Initialize a source selector."""
+        """Create a source selector.
+
+        Source selectors may have their own special arguments plus
+        any kwargs for lsst.pipe.base.Task
+        """
         pipeBase.Task.__init__(self, **kwargs)
 
-    def run(self, sourceCat, maskedImage=None, **kwargs):
-        """!Select sources and return them.
+    def run(self, source_cat, source_selected_field=None, masked_image=None,
+            matches=None):
+        """Select sources and return them.
 
-        @param[in] sourceCat  catalog of sources that may be sources (an lsst.afw.table.SourceCatalog)
-        @param[in] maskedImage  the maskedImage containing the sources, for plotting.
+        Parameters:
+        -----------
+        source_cat : lsst.afw.table.SourceCatalog
+            Catalog of sources that may be sources
+        source_selected_field : {None} lsst.afw.table.Key
+            Key specifying a location field to write to. If None then no data
+            is written to the output source cat.
+        masked_image : {None} lsst.afw.image.MaskedImage
+            Masked image containing the sources. A few source selectors may use
+            this in their selection but most will just use it for plotting.
+        matches : {None} list of lsst.afw.table.ReferenceMatch
+            A list of lsst.afw.table.ReferenceMatch objects. If use_matches set
+            in source selector, this field is required otherwise ignored.
 
-        @return an lsst.pipe.base.Struct containing:
-        - sourceCat  catalog of sources that were selected
+        Return
+        ------
+        lsst.pipe.base.Struct
+            The struct contains the following data:
+
+            source_cat : lsst.afw.table.SourceCatalog
+                The catalog of sources that were selected.
         """
-        return self.selectSources(maskedImage=maskedImage, sourceCat=sourceCat, **kwargs)
+        return self.select_sources(source_cat=source_cat,
+                                   source_selected_field=None,
+                                   masked_image=masked_image,
+                                   matches=matches)
 
     @abc.abstractmethod
-    def selectSources(self, sourceCat, matches=None):
-        """!Return a catalog of sources: a subset of sourceCat.
+    def select_sources(self, source_cat, source_selected_field=None,
+                       masked_image=None, matches=None):
+        """Return a catalog of sources selected by specified criteria.
 
-        @param[in] sourceCat  catalog of sources that may be sources (an lsst.afw.table.SourceCatalog)
+        We would prefer the input catalog to be contiguous in memory
+        for speed and simplicity of the code. However, the code does
+        allows for the input of non contiguous catalogs. Through
+        a different code path.
 
-        @return a pipeBase.Struct containing:
-        - sourceCat  a catalog of sources
+        Parameters
+        ----------
+        source_cat : lsst.afw.table.SourceCatalog
+            catalog of sources that may be sources
+        source_selected_field : {None} lsst.afw.table.Key
+            Key specifying a location field to write to
+            if the key was set.
+        masked_image : {None} lsst.afw.image
+            An image containing the sources tests or for plotting.
+        matches : {None} list of lsst.afw.table.ReferenceMatch
+            A list of lsst.afw.table.ReferenceMatch objects
+
+        Return
+        ------
+        lsst.pipe.base.Struct
+            The struct contains the following data:
+
+            result : lsst.afw.table.SourceCatalog
+                The catalog of sources that were selected.
         """
 
-        # NOTE: example implementation, returning all sources that have no bad flags set.
-        result = afwTable.SourceCatalog(sourceCat.table)
-        for source in sourceCat:
-            if not self._isBad(source):
-                result.append(source)
-        return pipeBase.Struct(sourceCat=result)
+        # NOTE: example implementation, returning all sources that have no bad
+        # flags set.
+        result = afwTable.SourceCatalog(source_cat.table)
 
-    def _isBad(self, source):
-        """Return True if any of config.badFlags are set for this source."""
-        return any(source.get(flag) for flag in self.config.badFlags)
+        is_bad_array = self._isBad(source_cat)
+        if source_selected_field is not None:
+            result.get(source_selected_field) = np.logical_not(
+                is_bad_array)
+            return pipeBase.Struct(source_cat=result)
+        return pipeBase.Struct(source_cat=result[np.logical_not(is_bad_array)])
+
+    def _is_bad(self, source):
+        """Return True if any of config.badFlags are set for this source.
+
+        Parameters
+        ----------
+        source : lsst.afw.tabel.SourceRecord
+            Source record object to test again the selection.
+
+        Return
+        ------
+        bool array
+            True if source fails any of the selections.
+        """
+        return any(source.get(flag) for flag in self.config.bad_flags)
+
 
 
 sourceSelectorRegistry = pexConfig.makeRegistry(
