62730ba8c6d4267fd330f0ed8a22e9da914f7cec
Nate Lust <nlust@astro.princeton.edu>
2017-06-21 15:57:43 -0400

    Transition to int32 for Masks
    The transition to int32 for Masks triggered a name change for masks.
    Update to the new api.

--- python/lsst/meas/algorithms/testUtils.py
+++ python/lsst/meas/algorithms/testUtils.py
@@ -1,8 +1,8 @@
-# 
+#
 # LSST Data Management System
 #
 # Copyright 2008-2017  AURA/LSST.
-# 
+#
 # This product includes software developed by the
 # LSST Project (http://www.lsst.org/).
 #
@@ -10,14 +10,14 @@
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
-# You should have received a copy of the LSST License Statement and 
-# the GNU General Public License along with this program.  If not, 
+#
+# You should have received a copy of the LSST License Statement and
+# the GNU General Public License along with this program.  If not,
 # see <https://www.lsstcorp.org/LegalNotices/>.
 #
 #!/usr/bin/env python
@@ -68,7 +68,7 @@ def plantSources(bbox, kwid, sky, coordList, addPoissonNoise=True):
         imgArr[:] = numpy.random.poisson(imgArr)
 
     # bundle into a maskedimage and an exposure
-    mask = afwImage.MaskU(bbox)
+    mask = afwImage.Mask(bbox)
     var = img.convertFloat()
     img -= sky
     mimg = afwImage.MaskedImageF(img.convertFloat(), mask, var)
