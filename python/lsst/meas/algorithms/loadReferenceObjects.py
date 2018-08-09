6c54d476e6bb5025be85ab2395538226525e97fa
davidstaker <david.staker@gmail.com>
2018-08-09 15:56:09 -0700

    test

--- python/lsst/meas/algorithms/loadReferenceObjects.py
+++ python/lsst/meas/algorithms/loadReferenceObjects.py
@@ -141,6 +141,7 @@ class LoadReferenceObjectsConfig(pexConfig.Config):
 
 
 class LoadReferenceObjectsTask(pipeBase.Task, metaclass=abc.ABCMeta):
+<<<<<<< HEAD
 ##<<<<<<< HEAD
 ####    """Abstract base class for tasks that load objects from a reference catalog
 ####    in a particular region of the sky.
@@ -214,6 +215,28 @@ class LoadReferenceObjectsTask(pipeBase.Task, metaclass=abc.ABCMeta):
 ##    resolved (optional): is the object spatially resolved?
 ##    variable (optional): does the object have variable brightness?"""
 ##>>>>>>> test
+    """Abstract base class for tasks that load objects from a reference catalog in a particular region of the sky.
+
+    Notes
+    -----
+    Reference object catalogs are instances of lsst.afw.table.SimpleCatalog with the following schema:
+    coord: ICRS position of star on sky (an lsst.geom.SpherePoint)
+    centroid: position of star on an exposure, if relevant (an lsst.afw.Point2D)
+    hasCentroid: is centroid usable?
+    referenceFilterName_flux: brightness in the specified reference catalog filter (Jy)
+    Note: the function lsst.afw.image.abMagFromFlux will convert flux in Jy to AB Magnitude.
+    referenceFilterName_fluxSigma (optional): brightness standard deviation (Jy);
+    omitted if no data is available; possibly nan if data is available for some objects but not others
+    camFlux: brightness in default camera filter (Jy); omitted if defaultFilter not specified
+    camFluxSigma: brightness standard deviation for default camera filter;
+    omitted if defaultFilter not specified or standard deviation not available that filter
+    cameraFilterName_camFlux: brightness in specified camera filter (Jy)
+    cameraFilterName_camFluxSigma (optional): brightness standard deviation
+    in specified camera filter (Jy); omitted if no data is available;
+    possibly nan if data is available for some objects but not others
+    photometric (optional): is the object usable for photometric calibration?
+    resolved (optional): is the object spatially resolved?
+    variable (optional): does the object have variable brightness?"""
     ConfigClass = LoadReferenceObjectsConfig
     _DefaultName = "LoadReferenceObjects"
 
