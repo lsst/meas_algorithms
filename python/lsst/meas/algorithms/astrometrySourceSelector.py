e50ace816aec2678de3690a3ebada174fb9ef76e
davidstaker <david.staker@gmail.com>
2018-08-13 15:57:39 -0700

    test

--- python/lsst/meas/algorithms/astrometrySourceSelector.py
+++ python/lsst/meas/algorithms/astrometrySourceSelector.py
@@ -76,27 +76,27 @@ class AstrometrySourceSelectorTask(BaseSourceSelectorTask):
         BaseSourceSelectorTask.__init__(self, *args, **kwargs)
 
     def selectSources(self, sourceCat, matches=None, exposure=None):
-        """Return a selection of sources that are useful for astrometry.
+        # """Return a selection of sources that are useful for astrometry.
 
-        Parameters:
-        -----------
-        sourceCat : `lsst.afw.table.SourceCatalog`
-            Catalog of sources to select from.
-            This catalog must be contiguous in memory.
-        matches : `list` of `lsst.afw.table.ReferenceMatch` or None
-            Ignored in this SourceSelector.
-        exposure : `lsst.afw.image.Exposure` or None
-            The exposure the catalog was built from; used for debug display.
+        # Parameters:
+        # -----------
+        # sourceCat : `lsst.afw.table.SourceCatalog`
+        #     Catalog of sources to select from.
+        #     This catalog must be contiguous in memory.
+        # matches : `list` of `lsst.afw.table.ReferenceMatch` or None
+        #     Ignored in this SourceSelector.
+        # exposure : `lsst.afw.image.Exposure`
+        #     The exposure the catalog was built from; used for debug display.
 
-        Return
-        ------
-        struct : `lsst.pipe.base.Struct`
-            The struct contains the following data:
+        # Return
+        # ------
+        # struct : `lsst.pipe.base.Struct`
+        #     The struct contains the following data:
 
-        selected : `array of bool'
-            Boolean array of sources that were selected, same length as
-            sourceCat.
-        """
+        # selected : `array of bool'
+        #     Boolean array of sources that were selected, same length as
+        #     sourceCat.
+        # """
         self._getSchemaKeys(sourceCat.schema)
 
         bad = reduce(lambda x, y: np.logical_or(x, sourceCat.get(y)), self.config.badFlags, False)
@@ -152,18 +152,18 @@ class AstrometrySourceSelectorTask(BaseSourceSelectorTask):
                 return sourceCat.get(self.fluxKey)/sourceCat.get(self.fluxErrKey) > self.config.minSnr
 
     def _isUsable(self, sourceCat):
-        """
-        Return True for each source that is usable for matching, even if it may
-        have a poor centroid.
+        # """
+        # Return True for each source that is usable for matching, even if it may
+        # have a poor centroid.
 
-        Notes
-        --------
-        For a source to be usable it must:
-        >> have a valid centroid
-        >> not be deblended
-        >> have a valid flux (of the type specified in this object's constructor)
-        >> have adequate signal-to-noise
-        """
+        # Notes
+        # --------
+        # For a source to be usable it must:
+        # >> have a valid centroid
+        # >> not be deblended
+        # >> have a valid flux (of the type specified in this object's constructor)
+        # >> have adequate signal-to-noise
+        # """
 
         return self._hasCentroid(sourceCat) \
             & ~self._isMultiple(sourceCat) \
@@ -171,17 +171,17 @@ class AstrometrySourceSelectorTask(BaseSourceSelectorTask):
             & ~sourceCat.get(self.fluxFlagKey)
 
     def _isGood(self, sourceCat):
-        """
-        Return True for each source that is usable for matching and likely has a
-        good centroid.
+        # """
+        # Return True for each source that is usable for matching and likely has a
+        # good centroid.
 
-        Notes
-        -------------
-        The additional tests for a good centroid, beyond isUsable, are:
-        >> not interpolated in the center
-        >> not saturated
-        >> not near the edge
-        """
+        # Notes
+        # -------------
+        # The additional tests for a good centroid, beyond isUsable, are:
+        # >> not interpolated in the center
+        # >> not saturated
+        # >> not near the edge
+        # """
 
         return self._isUsable(sourceCat) \
             & ~sourceCat.get(self.saturatedKey) \
