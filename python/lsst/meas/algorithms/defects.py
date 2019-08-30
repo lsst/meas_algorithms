e98e28a6b0e3fd42fd4a76b19923ad974577ae15
Tim Jenness <tjenness@lsst.org>
2019-08-30 11:35:10 -0700

    Remove deprecated policyToBadRegionList function

--- python/lsst/meas/algorithms/defects.py
+++ python/lsst/meas/algorithms/defects.py
@@ -27,7 +27,6 @@ __all__ = ("Defects",)
 import logging
 import itertools
 import collections.abc
-from deprecated.sphinx import deprecated
 import numpy as np
 import copy
 import datetime
@@ -37,7 +36,6 @@ import os.path
 import astropy.table
 
 import lsst.geom
-import lsst.pex.policy as policy
 import lsst.afw.table
 import lsst.afw.detection
 import lsst.afw.image
@@ -50,36 +48,6 @@ log = logging.getLogger(__name__)
 
 SCHEMA_NAME_KEY = "DEFECTS_SCHEMA"
 SCHEMA_VERSION_KEY = "DEFECTS_SCHEMA_VERSION"
-
-
-@deprecated(reason="Policy defect files no longer supported (will be removed after v18)",
-            category=FutureWarning)
-def policyToBadRegionList(policyFile):
-    """Given a Policy file describing a CCD's bad pixels, return a vector of BadRegion::Ptr"""
-
-    badPixelsPolicy = policy.Policy.createPolicy(policyFile)
-    badPixels = []
-
-    if badPixelsPolicy.exists("Defects"):
-        d = badPixelsPolicy.getArray("Defects")
-        for reg in d:
-            x0 = reg.get("x0")
-            width = reg.get("width")
-            if not width:
-                x1 = reg.get("x1")
-                width = x1 - x0 - 1
-
-            y0 = reg.get("y0")
-            if reg.exists("height"):
-                height = reg.get("height")
-            else:
-                y1 = reg.get("y1")
-                height = y1 - y0 - 1
-
-            bbox = lsst.geom.BoxI(lsst.geom.PointI(x0, y0), lsst.geom.ExtentI(width, height))
-            badPixels.append(Defect(bbox))
-
-    return badPixels
 
 
 class Defects(collections.abc.MutableSequence):
