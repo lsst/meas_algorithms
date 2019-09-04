0dcfc1223ae0191949a25fabfdd236b3b53f99ba
Tim Jenness <tjenness@lsst.org>
2019-09-04 13:04:58 -0700

    CCB advises that removal after 19.0 is acceptable

--- python/lsst/meas/algorithms/crLib.py
+++ python/lsst/meas/algorithms/crLib.py
@@ -10,7 +10,7 @@ def findCosmicRays(image, psf, bkgd, ps, keep=False):
     """Find cosmic rays in an image."""
     if isinstance(ps, pexPolicy.Policy):
         warnings.warn("findCosmicRays acceptance of Policy is deprecated. Use PropertySet "
-                      "(support will be removed after v20.0)",
+                      "(support will be removed after v19.0)",
                       category=FutureWarning, stacklevel=2)
         ps = ps.asPropertySet()
     return _findCosmicRays(image, psf, bkgd, ps, keep)
