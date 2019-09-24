dc3738d3d00dbf509f025d51432d020ca6bb0555
Simon Krughoff <krughoff@lsst.org>
2019-09-24 23:42:53 +0000

    Fix bugs with classmethods

--- python/lsst/meas/algorithms/simple_curve.py
+++ python/lsst/meas/algorithms/simple_curve.py
@@ -70,7 +70,8 @@ class Curve(ABC):
         table = Table.read(filename, format='fits')
         return cls.subclasses[table.meta['MODE']].fromTable(table)
 
-    def _check_cols(self, cols, table):
+    @staticmethod
+    def _check_cols(cols, table):
         for col in cols:
             if col not in table.columns.keys():
                 raise ValueError(f'The table must include a column named "{col}".')
@@ -112,9 +113,10 @@ class DetectorCurve(Curve):
         metadata.update({'MODE': 'DETECTOR', 'TYPE': 'QE'})
         self.metadata = metadata
 
-    def fromTable(self, table):
-        self._check_cols(['wavelength', 'efficiency'], table)
-        self.__init__(table['wavelength'], table['efficiency'], table.meta)
+    @classmethod
+    def fromTable(cls, table):
+        cls._check_cols(['wavelength', 'efficiency'], table)
+        return cls(table['wavelength'].quantity, table['efficiency'].quantity, table.meta)
 
     def toTable(self):
         return Table({'wavelength': self.wavelength, 'efficiency': self.efficiency}, meta=self.metadata)
@@ -128,27 +130,40 @@ class AmpCurve(Curve):
 
     def __init__(self, amp_name_list, wavelength, efficiency, metadata):
         super().__init__()
-        amp_names = set(amp_name_list)
+        amp_names = set(amp_name_list.flat)
+        self.data = {}
         for amp_name in amp_names:
-            idx = numpy.where(amp_name_list == 'amp_name')[0]
+            idx = numpy.where(amp_name_list == amp_name)[0]
             self.data[amp_name] = (wavelength[idx], efficiency[idx])
         # make sure needed metadata is set if built directly from ctor.
         metadata.update({'MODE': 'AMP', 'TYPE': 'QE'})
         self.metadata = metadata
 
-    def fromTable(self, table):
-        self._check_cols(['amp_name', 'wavelength', 'efficiency'], table)
-        self.__init__(table['amp_name'], table['wavelength'], table['efficiency'], table.meta)
+    @classmethod
+    def fromTable(cls, table):
+        cls._check_cols(['amp_name', 'wavelength', 'efficiency'], table)
+        return cls(table['amp_name'], table['wavelength'].quantity,
+                   table['efficiency'].quantity, table.meta)
 
     def toTable(self):
-        wavelength = numpy.array([])
-        efficiency = numpy.array([])
+        wavelength = None
+        efficiency = None
         names = numpy.array([])
         for amp_name, val in self.data.items():
-            wavelength = numpy.concatenate(wavelength, val[0])
-            efficiency = numpy.concatenate(efficiency, val[1])
-            names = numpy.concatenate(names, numpy.full(val[0].shape, amp_name))
-        return Table({'amp_name': names, 'wavelength': wavelength, 'efficiency': efficiency},
+            # This will be preserve quantity
+            if wavelength is None:
+                wunit = val[0].unit
+                wavelength = val[0].value
+            else:
+                wavelength = numpy.concatenate([wavelength, val[0].value])
+            if efficiency is None:
+                eunit = val[1].unit
+                efficiency = val[1].value
+            else:
+                efficiency = numpy.concatenate([efficiency, val[1].value])
+            names = numpy.concatenate([names, numpy.full(val[0].shape, amp_name)])
+        names = numpy.array(names)
+        return Table({'amp_name': names, 'wavelength': wavelength*wunit, 'efficiency': efficiency*eunit},
                      meta=self.metadata)
 
     def evaluate(self, detector, position, wavelength, kind='linear'):
