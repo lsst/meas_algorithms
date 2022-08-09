9c90e3546110ebe6d2445b9593341a4c2cef1f6d
John Parejko <parejkoj@uw.edu>
2022-08-09 16:35:02 -0700

    Add name arg to ReferenceObjectLoader
    Tasks that create a loader will pass the name in from their respective
    Input Connection.

--- tests/nopytest_ingestIndexReferenceCatalog.py
+++ tests/nopytest_ingestIndexReferenceCatalog.py
@@ -107,6 +107,7 @@ class TestConvertReferenceCatalogParallel(ingestIndexTestBase.ConvertReferenceCa
             loaderConfig = ReferenceObjectLoader.ConfigClass()
             loader = ReferenceObjectLoader([dataRef.dataId for dataRef in datasetRefs],
                                            handlers,
+                                           name="testrefcat",
                                            config=loaderConfig,
                                            log=self.logger)
             self.checkAllRowsInRefcat(loader, skyCatalog1, config)
