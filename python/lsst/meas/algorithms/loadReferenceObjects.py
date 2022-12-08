39297d633a09b8585cbfd98cdd0a8ff94a3e77f4
Merlin Fisher-Levine <merlin.fisherlevine@gmail.com>
2022-12-08 07:33:05 -0800

    Make_filterFunc a no-op

--- python/lsst/meas/algorithms/loadReferenceObjects.py
+++ python/lsst/meas/algorithms/loadReferenceObjects.py
@@ -835,6 +835,7 @@ class ReferenceObjectLoader:
             # beyond the projected detector of interest.  This step is required
             # due to the very local nature of the wcs available for the
             # sky <--> pixel conversions.
+            return refCat
             preFiltFunc = _FilterCatalog(outerSkyRegion)
             refCat = preFiltFunc(refCat, region)
 
