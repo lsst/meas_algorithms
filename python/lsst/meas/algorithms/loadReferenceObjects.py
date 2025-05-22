fad4c074ab3a9fba414110d1ed193fd435027911
Eli Rykoff <erykoff@stanford.edu>
2025-05-21 23:36:54 -0700

    Switch to vectorized bbox filtering.

--- python/lsst/meas/algorithms/loadReferenceObjects.py
+++ python/lsst/meas/algorithms/loadReferenceObjects.py
@@ -599,14 +599,10 @@ class ReferenceObjectLoader:
             # region defined by the inner sky region.
             if innerSkyRegion.contains(region):
                 return refCat
-            # Create a new reference catalog, and populate it only with records
-            # that fall inside the padded bbox.
-            filteredRefCat = type(refCat)(refCat.table)
-            centroidKey = afwTable.Point2DKey(refCat.schema['centroid'])
-            for record in refCat:
-                pixCoords = record[centroidKey]
-                if paddedBbox.contains(geom.Point2D(pixCoords)):
-                    filteredRefCat.append(record)
+
+            inside = paddedBbox.contains(x=refCat["slot_Centroid_x"], y=refCat["slot_Centroid_y"])
+            filteredRefCat = refCat[inside]
+
             return filteredRefCat
         return self.loadRegion(outerSkyRegion, filterName, filtFunc=_filterFunction, epoch=epoch)
 
