28995e637f8b91c233c00ca3adde0bc32ba0d7a1
Lee Kelvin <lkelvin@astro.princeton.edu>
2026-04-14 11:54:23 -0700

    Move brightStarStamps.py into pipe_tasks
    This script was previously located in meas_algorithms. The decision to
    move it into pipe_tasks was two-fold:
    
    1. Use of lsst.images format inside bright star stamps introduced a
    cyclical dependency issue. lsst.images depends on meas_extensions_psfex
    and meas_extensions_piff (optionally) because it has wrappers for those
    and needs to be able to import the legacy modules to test them. Moving
    the bright star stamp storage classes into pipe_tasks resolves this
    issue in the short term.
    
    2. Having the storage classes located alongside the consuming pipeline
    tasks helps to streamline development, keeping edits contained within a
    single package.
    
    N.B.: this commit essentially undoes the prior two commits. However,
    they are being persisted here to aid assessment of what edits were made
    to the original scripts.

--- python/lsst/meas/algorithms/__init__.py
+++ python/lsst/meas/algorithms/__init__.py
@@ -56,7 +56,6 @@ from .skyObjects import *
 from .dynamicDetection import *
 from .makePsfCandidates import *
 from .stamps import *
-from .brightStarStamps import *
 from .accumulator_mean_stack import *
 from .scaleVariance import *
 from .noise_covariance import *
